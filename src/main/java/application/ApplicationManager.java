a57d666796a181b1f0a9e69bcef8422ff4c66072
Borismit <86401120+Borismit@users.noreply.github.com>
2021-08-04 10:49:45 +0300

    added testing calendar

---
+++ src/main/java/application/ApplicationManager.java
@@ -0,0 +1,40 @@
+package application;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    WebDriver wd;
+    UserHelper userHelper;
+    CarHelper carHelper;
+    SearchHelper search;
+
+public void init(){
+    wd= new ChromeDriver();
+    wd.manage().window().maximize();
+    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    wd.navigate().to("https://ilcarro.xyz/search");
+    userHelper = new UserHelper(wd);
+    carHelper= new CarHelper(wd);
+    search = new SearchHelper(wd);
+
+}
+
+public void stop(){
+    wd.quit();
+}
+
+  public UserHelper userHelper() {
+       return userHelper;
+   }
+
+    public CarHelper carHelper() {
+        return carHelper;
+    }
+
+    public SearchHelper search() {
+        return search;
+    }
+}
